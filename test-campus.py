c8b7d5d6a6fbc8caaa498371fcbddd812017e466
Jared Ivey <gth856t@mail.gatech.edu>
2017-01-18 20:36:04 -0500

    Provide stitchedLink interface

--- test-campus.py
+++ test-campus.py
@@ -16,12 +16,11 @@ import geni.util
 import localcontext
 context = localcontext.buildContext()
 
-SLICE = "python-geni-lib"
+SLICE = "geni-desktop-slice"
 CTRLMASK = "255.255.255.252"
 NETMASK = "255.255.0.0"
-GATECH = set([IG.UtahDDC])
-CLEMSON = set([IG.UtahDDC])
-UKYPKS2 = set([IG.UtahDDC])
+UKYPKS2 = set([IG.UKYPKS2])
+CLEMSON = set([IG.Clemson])
 
 ovs_interfaces = 0
 ctrl_interfaces = 0
@@ -43,7 +42,7 @@ def createOvsSwitch(name, cmid, index):
     ovs.component_manager_id = cmid
     return ovs
 
-def createCtrlLink(ctrl, ovs, index):
+def createCtrlLink(ctrl, ovs, index, new_link=PG.LAN()):
     global ovs_interfaces
     ovs_to_ctrl_intf = ovs.addInterface("if%d" % (ovs_interfaces))
     ovs_interfaces += 1
@@ -54,7 +53,7 @@ def createCtrlLink(ctrl, ovs, index):
     ctrl_to_ovs_intf = ctrl.addInterface("if%d" % (ctrl_interfaces))
     ctrl_interfaces += 1
     ctrl_to_ovs_intf.addAddress(PG.IPv4Address("11.1.1.%d" % (4 * index + 2), CTRLMASK))
-    ctrl_link = PG.LAN()
+    ctrl_link = new_link
     ctrl_link.addInterface(ctrl_to_ovs_intf)
     ctrl_link.addInterface(ovs_to_ctrl_intf)
     ctrl_link.vlan_tagging = True
@@ -67,7 +66,7 @@ def createHost (name, cmid):
                               command ="sudo git clone https://github.com/jaredivey/geni-install-files /local/geni-install-files"))
     return vzc
 
-def createOvs2HostLink(ovs, host, index, i):
+def createOvs2HostLink(ovs, host, index, i, new_link=PG.LAN()):
     global ovs_interfaces
     ovs_intf = ovs.addInterface("if%d" % (ovs_interfaces))
     ovs_interfaces += 1
@@ -80,13 +79,13 @@ def createOvs2HostLink(ovs, host, index, i):
     host_interfaces += 1
     host_intf.addAddress(PG.IPv4Address("10.10.%d.%d" % (index,i), NETMASK))
 
-    link = PG.LAN()
+    link = new_link
     link.addInterface(ovs_intf)
     link.addInterface(host_intf)
     link.vlan_tagging = True
     return link
 
-def createOvs2OvsLink(ovs1, ovs2, subnet, index):
+def createOvs2OvsLink(ovs1, ovs2, subnet, index, new_link=PG.LAN()):
     global ovs_interfaces
     intf1 = ovs1.addInterface("if%d" % (ovs_interfaces))
     ovs_interfaces += 1
@@ -94,7 +93,7 @@ def createOvs2OvsLink(ovs1, ovs2, subnet, index):
     intf2 = ovs2.addInterface("if%d" % (ovs_interfaces))
     ovs_interfaces += 1
     intf2.addAddress(PG.IPv4Address("12.10.%d.%d" % (subnet, 2*index+2), NETMASK))
-    link = PG.LAN()
+    link = new_link
     link.addInterface(intf1)
     link.addInterface(intf2)
     link.vlan_tagging = True
@@ -102,11 +101,11 @@ def createOvs2OvsLink(ovs1, ovs2, subnet, index):
 
 r = PG.Request()
 for site in IG.aggregates():
-    if site not in GATECH:
+    if site not in UKYPKS2:
         continue
 
     print site.name
-    geni.util.deleteSliverExists(site, context, SLICE)
+    #geni.util.deleteSliverExists(site, context, SLICE)
 
     try:
         ad = site.listresources(context)
@@ -182,11 +181,11 @@ for site in IG.aggregates():
     subnet = 2
     ovs2 = []
     sn2_switches = 7
-    sn2_links = 8
+    sn2_links = 1
     for i in xrange(0,sn2_switches):
         ovs2.append(createOvsSwitch("ovs%d-%d" % (subnet, i), cmid, i))
         r.addResource(ovs2[i])
-        r.addResource(createCtrlLink(ctrl,ovs2[i],i+sn0_switches+sn1_switches))
+        r.addResource(createCtrlLink(ctrl,ovs2[i],i+sn0_switches+sn1_switches, new_link=PG.StitchedLink()))
 
         if i > 1:
             vzc = createHost("host%d-%d" % (subnet, i + 1), cmid)
@@ -231,7 +230,7 @@ for site in IG.aggregates():
     subnet = 3
     ovs3 = []
     sn3_switches = 4
-    sn3_links = 8
+    sn3_links = 1
     for i in xrange(0,sn3_switches):
         ovs3.append(createOvsSwitch("ovs%d-%d" % (subnet, i), cmid, i))
         r.addResource(ovs3[i])
@@ -260,17 +259,49 @@ for site in IG.aggregates():
     r.addResource(createOvs2OvsLink(ovs3[1], ovs3[3], subnet, 3))
     r.addResource(createOvs2OvsLink(ovs3[2], ovs3[3], subnet, 4))
 
+for site in IG.aggregates():
+    if site not in CLEMSON:
+        continue
+
+    print site.name
+    # geni.util.deleteSliverExists(site, context, SLICE)
+
+    try:
+        ad = site.listresources(context)
+    except Exception:
+        # Continue past aggregates that are down
+        print "%s might be down" % site.name
+        continue
+
+    cmid = ad.nodes[0].component_manager_id
+
     # Create subnet "4" (really just one switch connecting subnets 0 and 2
     print "Creating switch 4"
     subnet = 4
     sn4_switches = 1
     ovs4 = createOvsSwitch("ovs%d-%d" % (subnet, 0), cmid, 0)
     r.addResource(ovs4)
-    r.addResource(createCtrlLink(ctrl,ovs4,sn0_switches+sn1_switches+sn2_switches+sn3_switches))
+    r.addResource(createCtrlLink(ctrl,ovs4,sn0_switches+sn1_switches+sn2_switches+sn3_switches, new_link=PG.StitchedLink()))
 
-    r.addResource(createOvs2OvsLink(ovs4, ovs0[0], subnet, 1))
+    r.addResource(createOvs2OvsLink(ovs4, ovs0[0], subnet, 1, new_link=PG.StitchedLink()))
     r.addResource(createOvs2OvsLink(ovs4, ovs2[0], subnet, 2))
     r.addResource(createOvs2OvsLink(ovs4, ovs2[1], subnet, 3))
+
+for site in IG.aggregates():
+    if site not in UKYPKS2:
+        continue
+
+    print site.name
+    # geni.util.deleteSliverExists(site, context, SLICE)
+
+    try:
+        ad = site.listresources(context)
+    except Exception:
+        # Continue past aggregates that are down
+        print "%s might be down" % site.name
+        continue
+
+    cmid = ad.nodes[0].component_manager_id
 
     # Create subnet "5" (really just one switch connecting subnets 0 and 3 and switch 4
     print "Creating switch 5"
@@ -283,8 +314,8 @@ for site in IG.aggregates():
     r.addResource(createOvs2OvsLink(ovs5, ovs0[1], subnet, 1))
     r.addResource(createOvs2OvsLink(ovs5, ovs3[0], subnet, 2))
     r.addResource(createOvs2OvsLink(ovs5, ovs3[1], subnet, 3))
-    r.addResource(createOvs2OvsLink(ovs5, ovs4, subnet, 4))
+    r.addResource(createOvs2OvsLink(ovs5, ovs4, subnet, 4, new_link=PG.StitchedLink()))
 
-r.writeXML("ovs-campus.rspec")
+r.writeXML("ovs-ukypks2-clemson-stitched-campus.rspec")
 #m = site.createsliver(context, SLICE, r)
 #geni.util.printlogininfo(manifest=m)
